be5503943132f23dfc28209e086c245e844cbf08
Cauã Lincoln <95835911+CauaLincoln@users.noreply.github.com>
2022-09-30 11:39:45 -0300

    publicacao

---
+++ script.js
@@ -0,0 +1,13 @@
+const calcular = document.querySelector("#calcular")
+
+calcular.addEventListener("click", () => {
+    console.log("Fui clicado")
+})
+
+
+const elemento = document.querySelector("#calcular");
+const resultado = document.querySelector(".resultado");
+
+elemento.addEventListener("click", (evento) => {
+    resultado.innerHTML = "Fui clicado"
+})
